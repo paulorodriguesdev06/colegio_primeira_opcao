6c17b7ada3fff05acff64ad2be11283e0afec6d0
Paulo Victor <paulo.rodrigues.develop@gmail.com>
2025-10-21 15:49:30 -0300

    Adição da tag <i> para os icons, funcionamento do template e início de criação de um perfil-box para encerrar sessão.

--- app/views/templates/template.php
+++ app/views/templates/template.php
@@ -1,6 +1,8 @@
 <?php
-    namespace App\views\templates;
-    use App\Core\Controller;
+
+namespace App\views\templates;
+
+use App\Core\Controller;
 ?>
 
 <!DOCTYPE html>
@@ -16,108 +18,173 @@
     <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Story+Script&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <script src="<?= BASE_ASSETS ?>/js/jquery-3.7.1.min.js"></script>
+    <script src="https://kit.fontawesome.com/fb70a83e7a.js" crossorigin="anonymous"></script>
 </head>
 <script defer>
-    
+    $(document).ready(function() {
+
+        $('#tema').on('click', function() {
+            const $icon = $('#tema-icon');
+
+            $icon.addClass('opacity-0 rotate-180 transition-all duration-300'); // sai suavemente
+
+            setTimeout(() => {
+                if ($icon.hasClass('fa-sun')) {
+                    $icon.removeClass('fa-sun').addClass('fa-moon');
+                } else {
+                    $icon.removeClass('fa-moon').addClass('fa-sun');
+                }
+
+                $icon.removeClass('rotate-180');
+                $icon.removeClass('opacity-0').addClass('opacity-100');
+            }, 300); // mesma duração do fade
+        });
+
+        $('.nav-link').click(function() {
+            if ($(this).hasClass('dropdown')) {
+                $('#documents-nav').addClass('bg-blue-800');
+            }
+            $('.nav-link').removeClass('bg-blue-800');
+            $(this).toggleClass('bg-blue-800');
+        });
+
+
+        $('#documents-nav').on('click', function(e) {
+            e.preventDefault();
+
+            const $dropdown = $('#documents-dropdown');
+            const $arrow = $('#arrow-document');
+
+            if ($dropdown.hasClass('max-h-0')) {
+                // Abrir com transição suave
+                $dropdown.removeClass('max-h-0 opacity-0').addClass('max-h-40 opacity-100');
+                $arrow.addClass('rotate-180');
+            } else {
+                // Fechar com transição suave
+                $dropdown.removeClass('max-h-40 opacity-100').addClass('max-h-0 opacity-0');
+                $arrow.removeClass('rotate-180');
+            }
+        });
+    });
 </script>
 
 <body class="bg-gray-100 w-screen h-screen font-[Poppins] overflow-hidden">
+
     <header>
         <div class="relative">
-
-            <nav id="left-menu" class="flex flex-col fixed left-0 w-[15%] h-full bg-slate-600 shadow-lg text-white text-sm p-4">
-
-                <div class="absolute left-[94%] top-[7%] bg-white w-6 h-6 rounded-full text-black flex justify-center items-center">
-                    >
-                </div>
+            <nav id="left-menu" class="flex flex-col fixed left-0 w-60 h-full bg-blue-700 shadow-lg text-white text-sm p-4 ">
 
                 <div class="logo-box flex mb-[10%]">
-                    <img src="<?= BASE_IMAGES ?>logo-removebg.png?>" class="" alt="Logo-image">
-                    <h2>Colégio Primeira Opção</h2>
+                    <img src="<?= BASE_IMAGES ?>logo.jpg?>" class="max-w-[50px] rounded-full" alt="Logo-image">
+                    <h2 class="text-xl font-[Lobster] block pl-2">Colégio <span class="flex">Primeira Opção</span></h2>
                 </div>
 
                 <div class="nav-links flex flex-col gap-1">
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-700 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-casa-64.png" alt="">
-                        </div>
+                    <!-- Home -->
+
+                    <a href="#" class="nav-link flex gap-2 transition hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-regular fa-house text-lg text-gray-100"></i>
                         Home
                     </a>
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-calendário-64.png" alt="">
-                        </div>
+                    <!-- Calendário -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-regular fa-calendar-days text-lg text-gray-100"></i>
                         Calendário
                     </a>
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-funcionários-64.png" alt="">
-                        </div>
+                    <!-- Funcionários -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-solid fa-user-group text-lg text-gray-100"></i>
                         Funcionários
                     </a>
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-homem-estudante-64.png" alt="">
-                        </div>
+                    <!-- Alunos -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-regular fa-user text-lg text-gray-100"></i>
                         Alunos
                     </a>
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl" id="documents-nav">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-abrir-pasta-64.png" alt="">
-                        </div>
+                    <!-- Documentos -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl" id="documents-nav">
+                        <i class="fa-regular fa-folder-open text-lg text-gray-100 transition"></i>
                         Documentos
-                        <div class="max-w-3 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/seta-baixo.png" alt="">
-                        </div>
+                        <i class="fa-solid fa-chevron-down" id="arrow-document"></i>
                     </a>
-                    <div class="hidden" id="documents-options">
-                        <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                            <div class="max-w-5 flex items-center">
-                                <img src="<?= BASE_IMAGES ?>/icons/icons8-pasta-64.png" alt="">
-                            </div>
-                            Pasta 1
+
+                    <!-- Dropdown Documentos -->
+
+                    <div class="overflow-hidden max-h-0 opacity-0 transition-all duration-400 ease-in-out ml-4" id="documents-dropdown">
+
+                        <!-- Provas -->
+
+                        <a href="#" class="nav-link flex gap-2 items-center p-2 ml-4 transition delay-100 hover:bg-blue-800 rounded-xl dropdown">
+                            <i class="fa-regular fa-file-lines text-lg text-gray-100"></i>
+                            Provas
                         </a>
-                        <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                            <div class="max-w-5 flex items-center">
-                                <img src="<?= BASE_IMAGES ?>/icons/icons8-pasta-64.png" alt="">
-                            </div>
-                            Pasta 2
+
+                        <!-- Doc. Escolares -->
+
+                        <a href="#" class="nav-link flex gap-2 items-center p-2 ml-4 mt-1 transition delay-100 hover:bg-blue-800 rounded-xl dropdown">
+                            <i class="fa-regular fa-file-pdf text-lg text-gray-100"></i>
+                            Doc. Escolares
                         </a>
                     </div>
 
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-dinheiro-64.png" alt="">
-                        </div>
+                    <!-- Financeiro -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-regular fa-money-bill-1 text-lg text-gray-100"></i>
                         Financeiro
                     </a>
                 </div>
 
+                <!-- Div do rodape do menu -->
+
                 <div class="nav-footer h-full flex flex-col justify-end mb-4">
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-configurações-64.png" alt="">
-                        </div>
-                        Configurações
+
+                    <a href="#" id="tema" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
+                        <i id="tema-icon" class="fa-regular fa-sun text-lg text-gray-100 transition-all ease-in-out"></i>
+                        Tema
                     </a>
-                    <a href="#" class="flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
-                        <div class="max-w-5 flex items-center">
-                            <img src="<?= BASE_IMAGES ?>/icons/icons8-suporte-64.png" alt="">
-                        </div>
+
+                    <!-- Suporte -->
+
+                    <a href="#" class="nav-link flex gap-2 items-center transition delay-100 hover:bg-blue-800 p-2 rounded-xl">
+                        <i class="fa-solid fa-gear text-lg text-gray-100"></i>
                         Suporte
                     </a>
+
                 </div>
             </nav>
         </div>
+        <div id="top-menu" class="fixed top-0 w-[calc(100%-240px)] h-13 ml-60 px-6 flex justify-between items-center bg-slate-100 border-b border-b-gray-200 shadow text-gray-700">
+            <h2>Olá, Paulo</h2>
+            <div class="flex items-center gap-2">
+                <i class="fa-solid fa-chevron-down text-sm"></i>
+                <i class="fa-regular fa-user text-xl"></i>
+                <div class=" max-h-20 px-4 py-3 absolute top-13 right-8 flex bg-slate-100 border border-gray-200 text-sm text-nowrap rounded-br-2xl rounded-bl-xl">
+                    <div class="flex flex-col gap-2 w-full">
+                        <div class="hover:underline transition-all">
+                            <i class="fa-solid fa-circle-info"></i>
+                            <a href="#">Perfil</a>
+                        </div>
+                        <div class="hover:bg-slate-200 transition-all w-full">
+                            <i class="fa-solid fa-right-to-bracket"></i>
+                            <a href="#">Sair</a>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </header>
-    <main>
-        <?php
-        $controll = new Controller();
-        require_once $controll->view($view, $viewData); ?>
+    <main class="w-[calc(100%-240px)] h-[calc(100%-52px)] ml-60 pt-17 px-4">
+
+        <?php $this->view($view, $viewData); ?>
+
     </main>
-    
